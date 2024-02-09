4b0d9ec49ac3f369cadaaffadcc5fa181e0dd624
jmparker2112@gmail.com <jmparker2112@gmail.com>
2024-02-08 18:31:50 -0600

    This is the initial backup of my Promineo Tech class work

---
+++ 2024-01-31-be-central/src/Week01/HelloWorld.java
@@ -0,0 +1,17 @@
+package Week01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// This is a comment. 
+		
+		/* 
+		 * This is also a comment
+		 */
+		
+		System.out.println("Hello world!!!");
+		System.out.println("Java Version: " + System.getProperty("Java.version"));
+		}
+}
